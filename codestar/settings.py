dd217f0e2880190c2e5321b13b90f60a516b73fd
Michael Lawlor <mikelawlorarb@outlook.com>
2024-07-20 12:57:06 +0000

    heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pn)_5fo^bl&%75zms4#y+9gs!5@l*#v@-zr*+r*-4g6sfkx+e9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition
